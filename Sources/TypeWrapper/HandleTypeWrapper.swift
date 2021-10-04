56d3acb3d5c358820b1995f39a9a567a47fa4f55
Joe Hinkle <joehinkle11@gmail.com>
2021-10-04 17:10:31 +0200

    idea of only types

--- Sources/TypeWrapper/HandleTypeWrapper.swift
+++ Sources/TypeWrapper/HandleTypeWrapper.swift
@@ -22,4 +22,20 @@ public struct HandleTypeWrapper<Wrapped> {
             throw TypeWrapperError.mismatch
         }
     }
+    func sendReturnOnlyType(_ input: Any, toKnown: (Any) -> TypeOnRecieve2?) throws -> TypeWrapper {
+        let attempt = AttemptIfConformsStruct(Wrapped.self, _NonGeneric.self)
+        if let typeOnRecieve = toKnown(attempt) {
+            return try typeOnRecieve(input)
+        } else {
+            throw TypeWrapperError.mismatch
+        }
+    }
+    func sendReturnOnlyType(_ input: Any, toKnown: (Any) -> TypeOnRecieve2?) throws -> TypeWrapper where Wrapped: _GenericRegister {
+        let attempt = AttemptIfConformsStruct(Wrapped.self, Wrapped.self)
+        if let typeOnRecieve = toKnown(attempt) {
+            return try typeOnRecieve(input)
+        } else {
+            throw TypeWrapperError.mismatch
+        }
+    }
 }
--- Sources/TypeWrapper/TypeWrapper.swift
+++ Sources/TypeWrapper/TypeWrapper.swift
@@ -1,10 +1,14 @@
 
 public typealias TypeOnRecieve = (_ input: Any) throws -> AnyWithTypeWrapper
+public typealias TypeOnRecieve2 = (_ input: Any) throws -> TypeWrapper
 
 public struct TypeWrapper {
     // public
     public func send(_ input: Any, as toKnown: (Any) -> TypeOnRecieve?) throws -> AnyWithTypeWrapper {
         try _send(input, toKnown)
+    }
+    public func sendReturnOnlyType(_ input: Any, as toKnown: (Any) -> TypeOnRecieve2?) throws -> TypeWrapper {
+        try _sendReturnOnlyType(input, toKnown)
     }
     #if DEBUG
     public let _rawType: Any.Type
@@ -12,16 +16,19 @@ public struct TypeWrapper {
     
     // private
     private let _send: (Any, (Any) -> TypeOnRecieve?) throws -> AnyWithTypeWrapper
+    private let _sendReturnOnlyType: (Any, (Any) -> TypeOnRecieve2?) throws -> TypeWrapper
     
     // inits
     public init<Wrapped>(withType type: Wrapped.Type) {
         self._send = HandleTypeWrapper<Wrapped>().send(_:toKnown:)
+        self._sendReturnOnlyType = HandleTypeWrapper<Wrapped>().sendReturnOnlyType(_:toKnown:)
         #if DEBUG
         self._rawType = Wrapped.self
         #endif
     }
     public init<Wrapped: _GenericRegister>(withType type: Wrapped.Type) {
         self._send = HandleTypeWrapper<Wrapped>().send(_:toKnown:)
+        self._sendReturnOnlyType = HandleTypeWrapper<Wrapped>().sendReturnOnlyType(_:toKnown:)
         #if DEBUG
         self._rawType = Wrapped.self
         #endif
